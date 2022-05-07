41268f65a078b5febe891ce514a4e6df9a3a5476
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-07 23:53:54 +0530

    Add dark text theme

--- lib/fooderlich_theme.dart
+++ lib/fooderlich_theme.dart
@@ -14,4 +14,17 @@ class FooderlichTheme {
     headline4: GoogleFonts.openSans(
         fontSize: 20.0, fontWeight: FontWeight.w600, color: Colors.black),
   );
+
+  static TextTheme darkTextTheme = TextTheme(
+    bodyText1: GoogleFonts.openSans(
+        fontSize: 14.0, fontWeight: FontWeight.w700, color: Colors.white),
+    headline1: GoogleFonts.openSans(
+        fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.white),
+    headline2: GoogleFonts.openSans(
+        fontSize: 21.0, fontWeight: FontWeight.w700, color: Colors.white),
+    headline3: GoogleFonts.openSans(
+        fontSize: 16.0, fontWeight: FontWeight.w600, color: Colors.white),
+    headline4: GoogleFonts.openSans(
+        fontSize: 20.0, fontWeight: FontWeight.w600, color: Colors.white),
+  );
 }
