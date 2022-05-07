120821cedc8d6791cef353090906e7bfa44c0979
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-07 23:37:07 +0530

    Change stateful widget to stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FooderLich());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
+class FooderLich extends StatelessWidget {
+  const FooderLich({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Fooder Lich'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        title: Text(widget.title),
-      ),
-      body: SafeArea(
-        child: Container(
-          color: Colors.deepPurpleAccent,
-          child: Column(
-            children: const [
-              Text('asd'),
-              Text('asd'),
-              Text('asd'),
-            ],
-          ),
+      title: 'Fooder Lich',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fooderlich'),
+        ),
+        body: const Center(
+          child: Text('Let\'s get cooking'),
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
