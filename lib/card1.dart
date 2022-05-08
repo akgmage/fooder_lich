9dd82c636c1912098c38c4442c5361d832c7993c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-08 18:32:39 +0530

    Add Card1 widget with blank container and few strings, Replace first Container in pages list with Card1

---
+++ lib/card1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Card1 extends StatelessWidget {
+  const Card1({Key? key}) : super(key: key);
+
+  final String category = 'Editor\'s Choice';
+  final String title = 'The Art of Dough';
+  final String description = 'Learn to make the perfect bread';
+  final String chef = 'Chef Extra';
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(),
+    );
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooder_lich/card1.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,7 +11,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int _selectedIndex = 0;
   static List<Widget> pages = <Widget>[
-    Container(color: Colors.red),
+    const Card1(),
     Container(color: Colors.green),
     Container(color: Colors.blue),
   ];
