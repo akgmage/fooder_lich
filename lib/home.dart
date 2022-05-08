df97f7abc97e55638a6166fb77987f3b6d44c8bb
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-08 18:21:40 +0530

    Add BottomNavigationBar & replace the body with static List of Widget - pages

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,18 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  static List<Widget> pages = <Widget>[
+    Container(color: Colors.red),
+    Container(color: Colors.green),
+    Container(color: Colors.blue),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,11 +28,25 @@ class _HomeState extends State<Home> {
             // 2
             style: Theme.of(context).textTheme.headline6),
       ),
-      body: Center(
-        child: Text(
-          'Lets get cooking',
-          style: Theme.of(context).textTheme.headline1,
-        ),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card2',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card2',
+          ),
+        ],
       ),
     );
   }
