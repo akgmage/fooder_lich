71fc36ea06c3d1e335ef2bff16b218c62ccb9deb
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-09 23:52:56 +0530

    Add AuthorCard with three properties authorNname title and imageProvider

---
+++ lib/author_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'circle_image.dart';
+
+class AuthorCard extends StatelessWidget {
+  final String authorName;
+  final String title;
+  final ImageProvider imageProvider;
+  const AuthorCard(
+      {Key? key,
+      required this.authorName,
+      required this.title,
+      required this.imageProvider})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        children: [
+          CircleImage(imageProvider: imageProvider, imageRadius: 28),
+          const SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                authorName,
+                style: FooderlichTheme.lightTextTheme.headline2,
+              ),
+              Text(
+                title,
+                style: FooderlichTheme.lightTextTheme.headline3,
+              ),
+            ],
+          ),
+          IconButton(
+            icon: const Icon(Icons.favorite_border),
+            iconSize: 30,
+            color: Colors.grey[400],
+            onPressed: () {
+              const snackBar = SnackBar(content: Text('Press Favirite'));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
