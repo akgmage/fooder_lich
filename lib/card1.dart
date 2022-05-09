89b8ea444fcd9e485e2d596e07430278781182f0
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-09 22:32:31 +0530

    Add text as stack int the Contianer

--- lib/card1.dart
+++ lib/card1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
 
 class Card1 extends StatelessWidget {
   const Card1({Key? key}) : super(key: key);
@@ -19,6 +20,37 @@ class Card1 extends StatelessWidget {
             fit: BoxFit.cover,
           ),
           borderRadius: BorderRadius.all(Radius.circular(11.0)),
+        ),
+        child: Stack(
+          children: [
+            Text(
+              category,
+              style: FooderlichTheme.darkTextTheme.bodyText1,
+            ),
+            Positioned(
+              child: Text(
+                title,
+                style: FooderlichTheme.darkTextTheme.headline5,
+              ),
+              top: 20,
+            ),
+            Positioned(
+              child: Text(
+                description,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 30,
+              right: 0,
+            ),
+            Positioned(
+              child: Text(
+                chef,
+                style: FooderlichTheme.darkTextTheme.bodyText1,
+              ),
+              bottom: 10,
+              right: 0,
+            ),
+          ],
         ),
       ),
     );
