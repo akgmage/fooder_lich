8afeadc99cb2449d7cfefb3f08e617660b644f77
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-10 00:27:09 +0530

    Add card3 with Container and Boxdecoration to display image with rounded corners

---
+++ lib/card3.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+
+class Card3 extends StatelessWidget {
+  const Card3({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext) {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints.expand(height: 450, width: 350),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag2.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+        child: Stack(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
