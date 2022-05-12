72f794b35e30e6b8bf2602aa046cec0cf9a5fe24
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-12 23:40:06 +0530

    Move components inside components directory

--- lib/components/author_card.dart
+++ lib/components/author_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'fooderlich_theme.dart';
+import '../fooderlich_theme.dart';
 import 'circle_image.dart';
 
 class AuthorCard extends StatefulWidget {
